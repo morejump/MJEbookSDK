b0b084879f765723b729ab4de0f13a470be39b24
thaoluuvan <luu.van.thao@framgia.com>
2019-05-09 17:53:38 +0700

    Added Dao

--- folioreader/src/main/java/com/mjebooksdk/model/dao/IReadLocationDao.java
+++ folioreader/src/main/java/com/mjebooksdk/model/dao/IReadLocationDao.java
@@ -2,9 +2,11 @@ package com.mjebooksdk.model.dao;
 
 import com.mjebooksdk.model.ReadLocation;
 
+import java.util.List;
+
 public interface IReadLocationDao {
     void add(ReadLocation readLocation);
     void update(ReadLocation readLocation);
     void delete(String id);
-    void getAll();
+    List<ReadLocation> getAll();
 }
--- folioreader/src/main/java/com/mjebooksdk/model/dao/MockReadLocationImpl.java
+++ folioreader/src/main/java/com/mjebooksdk/model/dao/MockReadLocationImpl.java
@@ -2,17 +2,19 @@ package com.mjebooksdk.model.dao;
 
 import com.mjebooksdk.model.ReadLocation;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-public class ReadLocationImpl implements IReadLocationDao {
+public class MockReadLocationImpl implements IReadLocationDao {
+    List<ReadLocation> readLocations = new ArrayList<>();
     @Override
     public void add(ReadLocation readLocation) {
-
+        readLocations.add(readLocation);
     }
 
     @Override
     public void update(ReadLocation readLocation) {
-
     }
 
     @Override
@@ -22,8 +24,6 @@ public class ReadLocationImpl implements IReadLocationDao {
 
     @Override
     public List<ReadLocation> getAll() {
-        return null;
+        return readLocations;
     }
-
-
 }
