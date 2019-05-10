096b101c54a24ab8d43bf0c0897f4463bbe1615d
thaoluuvan <luu.van.thao@framgia.com>
2019-05-10 14:48:25 +0700

    Added Bookmark Adapter

---
+++ mjebooksdk/src/main/java/com/mjebooksdk/ui/adapter/BookmarkAdapter.java
@@ -0,0 +1,48 @@
+package com.mjebooksdk.ui.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.mjebooksdk.database.ReadLocation;
+
+import java.util.List;
+
+public class BookmarkAdapter extends RecyclerView.Adapter<BookmarkAdapter.BookmarkViewHolder> {
+    List<ReadLocation> data;
+
+    public BookmarkAdapter(List<ReadLocation> data){
+        this.data = data;
+    }
+    @NonNull
+    @Override
+    public BookmarkViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookmarkViewHolder holder, int position) {
+        holder.bindData(data.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public void setData(List<ReadLocation> data){
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    public class BookmarkViewHolder extends RecyclerView.ViewHolder {
+
+        public BookmarkViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bindData(ReadLocation readLocation){
+
+        }
+    }
+}
