30e0e81d6a5c8eb82c160e8a2290fd0fe46db0c7
thaoluuvan <luu.van.thao@framgia.com>
2019-05-10 10:01:30 +0700

    Add Settings Activity

---
+++ folioreader/src/main/java/com/mjebooksdk/ui/activity/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.mjebooksdk.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mjebooksdk.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
