66586dbce5773e5439841599d6e2c830f637eb65
thaoluuvan <luu.van.thao@framgia.com>
2019-05-14 17:40:11 +0700

    show/hide notification text in Bookmark screen

--- mjebooksdk/src/main/java/com/mjebooksdk/ui/activity/BookmarkActivity.java
+++ mjebooksdk/src/main/java/com/mjebooksdk/ui/activity/BookmarkActivity.java
@@ -2,6 +2,8 @@ package com.mjebooksdk.ui.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
@@ -18,6 +20,8 @@ import com.mjebooksdk.ui.dialogs.DeleteBookmarkDialog;
 import com.mjebooksdk.ui.dialogs.UpdateBookmarkDialog;
 import com.mjebooksdk.util.DatabaseManager;
 
+import java.util.List;
+
 public class BookmarkActivity extends AppCompatActivity implements IBookmarkListener {
 
     private RecyclerView rvBookmark;
@@ -26,11 +30,14 @@ public class BookmarkActivity extends AppCompatActivity implements IBookmarkList
     private DeleteBookmarkDialog deleteBookmarkDialog;
     private UpdateBookmarkDialog updateBookmarkDialog;
     public static String BOOKMARK_RESULT_KEY = "BOOKMARK_RESULT_KEY";
+    public List<ReadLocation> readLocations;
+    public TextView txtNoItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookmark);
+        txtNoItem = findViewById(R.id.txtNoItem);
         updateBookmarkDialog = new UpdateBookmarkDialog(this);
         deleteBookmarkDialog = new DeleteBookmarkDialog(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -38,7 +45,9 @@ public class BookmarkActivity extends AppCompatActivity implements IBookmarkList
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         readLocationDao = DatabaseManager.getInstance();
-        bookmarkAdapter = new BookmarkAdapter(readLocationDao.getAllReadLocation(), this);
+        readLocations = readLocationDao.getAllReadLocation();
+        doShowHideNoti();
+        bookmarkAdapter = new BookmarkAdapter(readLocations, this);
         rvBookmark = findViewById(R.id.rvBookmark);
         rvBookmark.setLayoutManager(new LinearLayoutManager(this));
         rvBookmark.setAdapter(bookmarkAdapter);
@@ -58,7 +67,9 @@ public class BookmarkActivity extends AppCompatActivity implements IBookmarkList
             @Override
             public void deleteBookmark() {
                 readLocationDao.deleteReadLoctionById(readLocation.getId());
-                bookmarkAdapter.setData(readLocationDao.getAllReadLocation());
+                readLocations = readLocationDao.getAllReadLocation();
+                doShowHideNoti();
+                bookmarkAdapter.setData(readLocations);
             }
         });
     }
@@ -70,7 +81,9 @@ public class BookmarkActivity extends AppCompatActivity implements IBookmarkList
             @Override
             public void updateBookmark(String title) {
                 readLocationDao.updateReadLocationByTitle(readLocation, title);
-                bookmarkAdapter.setData(readLocationDao.getAllReadLocation());
+                readLocations = readLocationDao.getAllReadLocation();
+                doShowHideNoti();
+                bookmarkAdapter.setData(readLocations);
             }
         });
 
@@ -82,5 +95,14 @@ public class BookmarkActivity extends AppCompatActivity implements IBookmarkList
         returnIntent.putExtra(BOOKMARK_RESULT_KEY, readLocation.getId());
         setResult(Activity.RESULT_OK, returnIntent);
         finish();
+    }
+
+    private void doShowHideNoti(){
+        if (readLocations.size() == 0) {
+            txtNoItem.setVisibility(View.VISIBLE);
+        }
+        else {
+            txtNoItem.setVisibility(View.GONE);
+        }
     }
 }
